bbd72f67e53843b3565256fe75d3d69185922638
TE24B-Ligandereklof-Anton <anton.ligandereklof@elev.ga.ntig.se>
2026-03-18 12:28:05 +0100

    Fixed EntityDirectionHandler so that the healthbars now rotate don't get mirrored when turning around

--- Assets/Scripts/EntityDirectionHandler.cs
+++ Assets/Scripts/EntityDirectionHandler.cs
@@ -8,18 +8,28 @@ using UnityEngine.UI;
 public class EntityDirectionHandler : MonoBehaviour
 {
     Canvas canvas;
-    float defaultXScale;
+    float defaultRotation; //should always be +/- 1
     void Start()
     {
-        defaultXScale = transform.localScale.x;
+        if (transform.localScale.x < 0) defaultRotation = -1;
+        else defaultRotation = 1;
+
         canvas = GetComponentInChildren<Canvas>();
     }
     public void SetRotation(int rotation)
     {
+        float absoluteX = Mathf.Abs(transform.localScale.x);
+        
         transform.localScale = new(
-            defaultXScale*rotation,
+            defaultRotation*rotation*absoluteX,
             transform.localScale.y,
             transform.localScale.z
+        );
+
+        canvas.gameObject.transform.localScale = new(
+            math.abs(canvas.gameObject.transform.localScale.x)*rotation,
+            canvas.gameObject.transform.localScale.y,
+            canvas.gameObject.transform.localScale.z
         );
 
     }
